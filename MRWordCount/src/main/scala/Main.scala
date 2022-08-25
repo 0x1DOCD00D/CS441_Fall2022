1a2bc026af894a2a6e71c41ee1b6a8af886f1b1b
Mark Grechanik <drmark@uic.edu>
2022-08-25 09:49:10 -0500

    map/reduce example project

---
+++ MRWordCount/src/main/scala/Main.scala
@@ -0,0 +1,47 @@
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.conf.*
+import org.apache.hadoop.io.*
+import org.apache.hadoop.util.*
+import org.apache.hadoop.mapred.*
+
+import java.io.IOException
+import java.util
+import scala.jdk.CollectionConverters.*
+/*
+* https://towardsdatascience.com/installing-hadoop-on-a-mac-ec01c67b003c
+* https://medium.com/analytics-vidhya/testing-your-hadoop-program-with-maven-on-intellij-42d534db7974
+* */
+
+object Main:
+  class Map extends MapReduceBase with Mapper[LongWritable, Text, Text, IntWritable]:
+    private final val one = new IntWritable(1)
+    private val word = new Text()
+
+    @throws[IOException]
+    def map(key: LongWritable, value: Text, output: OutputCollector[Text, IntWritable], reporter: Reporter): Unit =
+      val line: String = value.toString
+      line.split(" ").foreach { token =>
+        word.set(token)
+        output.collect(word, one)
+      }
+
+  class Reduce extends MapReduceBase with Reducer[Text, IntWritable, Text, IntWritable]:
+    override def reduce(key: Text, values: util.Iterator[IntWritable], output: OutputCollector[Text, IntWritable], reporter: Reporter): Unit =
+      val sum = values.asScala.reduce((valueOne, valueTwo) => new IntWritable(valueOne.get() + valueTwo.get()))
+      output.collect(key,  new IntWritable(sum.get()))
+
+  @main def runit(inputPath: String, outputPath: String) =
+    require(!inputPath.isBlank && !outputPath.isBlank)
+    println(inputPath)
+    val conf: JobConf = new JobConf(this.getClass)
+    conf.setJobName("WordCount")
+    conf.setOutputKeyClass(classOf[Text])
+    conf.setOutputValueClass(classOf[IntWritable])
+    conf.setMapperClass(classOf[Map])
+    conf.setCombinerClass(classOf[Reduce])
+    conf.setReducerClass(classOf[Reduce])
+    conf.setInputFormat(classOf[TextInputFormat])
+    conf.setOutputFormat(classOf[TextOutputFormat[Text, IntWritable]])
+    FileInputFormat.setInputPaths(conf, new Path(inputPath))
+    FileOutputFormat.setOutputPath(conf, new Path(outputPath))
+    JobClient.runJob(conf)
